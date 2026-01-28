1f8488043332a9fb823d3b8def08f40991fcccc5
Alexandru Scripchin <sripchin.sasa@gmail.com>
2026-01-28 19:37:29 +0200

    Add last changes

--- liste1ex.py
+++ liste1ex.py
@@ -1,1 +1,12 @@
-print("liste")
+numbers = [10, 20, 30, 40]
+print(numbers)
+print(numbers[3])
+
+print(numbers)
+numbers.append(100)
+numbers.append(200)
+
+print(numbers)
+
+numbers.pop(1)
+print(numbers)
---
+++ lkisteex2.py
@@ -0,0 +1,8 @@
+numbers = [10, 20, 30, 40]
+for number in numbers:
+    print(number)
+
+
+
+state_ale_lumii = ['franta','italia','belgia']
+print(state_ale_lumii)
